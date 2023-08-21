a7a6adaf8da41d616dd0308a2852afea721c6fd9
Van Chi Hieu <vanchihieu3@gmail.com>
2023-08-21 21:24:59 +0700

    Code logic filter theo category cho AsideFilter

--- src/pages/ProductList/AsideFilter/AsideFilter.tsx
+++ src/pages/ProductList/AsideFilter/AsideFilter.tsx
@@ -1,13 +1,27 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { Link, createSearchParams } from "react-router-dom";
 import Button from "src/components/Button";
 import Input from "src/components/Input";
 import path from "src/constants/path";
+import { QueryConfig } from "../ProductList";
+import { Category } from "src/types/category.type";
+import classNames from "classnames";
 
-export default function AsideFilter() {
+interface Props {
+    queryConfig: QueryConfig;
+    categories: Category[];
+}
+
+export default function AsideFilter({ queryConfig, categories }: Props) {
+    const { category } = queryConfig;
+
     return (
         <div className="py-4">
-            <Link to={path.home} className="flex items-center font-bold">
+            <Link
+                to={path.home}
+                className={classNames("flex items-center font-bold", {
+                    "text-orange": !category,
+                })}
+            >
                 <svg viewBox="0 0 12 10" className="mr-3 h-4 w-3 fill-current">
                     <g fillRule="evenodd" stroke="none" strokeWidth={1}>
                         <g transform="translate(-373 -208)">
@@ -25,25 +39,35 @@ export default function AsideFilter() {
             </Link>
             <div className="bg-gray-300 h-[1px] my-4 "></div>
             <ul>
-                <li className="py-2 pl-2">
-                    <Link
-                        to={path.home}
-                        className="relative px-2 text-orange font-semibold"
-                    >
-                        <svg
-                            viewBox="0 0 4 7"
-                            className="absolute top-1 left-[-10px] h-2 w-2 fill-orange"
-                        >
-                            <polygon points="4 3.5 0 0 0 7" />
-                        </svg>
-                        Thời trang nam
-                    </Link>
-                </li>
-                <li className="py-2 pl-2">
-                    <Link to={path.home} className="relative px-2 ">
-                        Điện thoại
-                    </Link>
-                </li>
+                {categories.map((categoryItem) => {
+                    const isActive = category === categoryItem._id;
+                    return (
+                        <li className="py-2 pl-2" key={categoryItem._id}>
+                            <Link
+                                to={{
+                                    pathname: path.home,
+                                    search: createSearchParams({
+                                        ...queryConfig,
+                                        category: categoryItem._id,
+                                    }).toString(),
+                                }}
+                                className={classNames(" relative px-2", {
+                                    "text-orange font-semibold": isActive,
+                                })}
+                            >
+                                {isActive && (
+                                    <svg
+                                        viewBox="0 0 4 7"
+                                        className="absolute top-1 left-[-10px] h-2 w-2 fill-orange"
+                                    >
+                                        <polygon points="4 3.5 0 0 0 7" />
+                                    </svg>
+                                )}
+                                {categoryItem.name}
+                            </Link>
+                        </li>
+                    );
+                })}
             </ul>
             <Link
                 to={path.home}
