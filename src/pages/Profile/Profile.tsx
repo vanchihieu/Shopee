f675c5816eb674bb463fa261954d5b5879339743
Van Chi Hieu <vanchihieu3@gmail.com>
2023-08-29 10:01:09 +0700

    Code UI trang Profile

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,6 +1,113 @@
+import Input from "src/components/Input";
 
 export default function Profile() {
-  return (
-    <div>Profile</div>
-  )
+    return (
+        <div className="rounded-sm bg-white px-2 pb-10 md:px-7 md:pb-20 shadow ">
+            <div className="border-b border-b-gray-200 py-6">
+                <h1 className="text-lg font-medium capitalize text-gray-900">
+                    Hồ sơ của tôi
+                </h1>
+                <div className="mt-1 text-sm text-gray-700">
+                    Quản lý thông tin hồ sơ để bảo mật tài khoản
+                </div>
+            </div>
+
+            <div className="mt-8 flex flex-col-reverse md:flex-row md:items-start">
+                <form className="mt-6 flex-grow pr-12 md:mt-0 md:pr-12">
+                    <div className="flex flex-wrap flex-col sm:flex-row">
+                        <div className="sm:w-[20%] truncate pt-3 sm:text-right capitalize">
+                            Email
+                        </div>
+                        <div className="sm:w-[80%] sm:pl-5">
+                            <div className="pt-3 text-gray-700">
+                                vanchihieu@gmail.com
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="mt-6 flex flex-wrap flex-col sm:flex-row">
+                        <div className="sm:w-[20%] truncate pt-3 sm:text-right capitalize">
+                            Tên
+                        </div>
+                        <div className="sm:w-[80%] sm:pl-5">
+                            <Input
+                                classNameInput="w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm"
+                                name="address"
+                                placeholder="Địa chỉ"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="mt-2 flex flex-wrap flex-col sm:flex-row">
+                        <div className="sm:w-[20%] truncate pt-3 sm:text-right capitalize">
+                            Số điẹn thoại
+                        </div>
+                        <div className="sm:w-[80%] sm:pl-5">
+                            <Input
+                                classNameInput="w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm"
+                                name="address"
+                                placeholder="Địa chỉ"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="mt-2 flex flex-wrap flex-col sm:flex-row">
+                        <div className="sm:w-[20%] truncate pt-3 sm:text-right capitalize">
+                            Địa chỉ
+                        </div>
+                        <div className="sm:w-[80%] sm:pl-5">
+                            <Input
+                                classNameInput="w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm"
+                                name="address"
+                                placeholder="Địa chỉ"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="mt-2 flex flex-wrap flex-col sm:flex-row">
+                        <div className="sm:w-[20%] truncate pt-3 sm:text-right capitalize">
+                            Ngày sinh
+                        </div>
+                        <div className="sm:w-[80%] sm:pl-5">
+                            <div className="flex justify-between">
+                                <select className="h-10 w-[32%] rounded-sm border border-black/10 px-3">
+                                    <option disabled>Ngày</option>
+                                </select>
+                                <select className="h-10 w-[32%] rounded-sm border border-black/10 px-3">
+                                    <option disabled>Tháng</option>
+                                </select>
+                                <select className="h-10 w-[32%] rounded-sm border border-black/10 px-3">
+                                    <option disabled>Năm</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+
+                <div className="flex justify-center md:w-72 md:border-l md:border-l-gray-200 ">
+                    <div className="flex flex-col items-center">
+                        <div className="my-5 h-24 w-24">
+                            <img
+                                src=""
+                                alt="ima"
+                                className="w-full h-full rounded-full object-cover"
+                            />
+                        </div>
+                        <input
+                            type="file"
+                            accept=".jpg, .jpeg, .png"
+                            className="hidden"
+                        />
+                        <button className="flex h-10 items-center justify-end rounded-sm border bg-white px-6 text-sm text-gray-600 shadow-sm">
+                            Chọn ảnh
+                        </button>
+                        <div className="mt-3 text-gray-400">
+                            <div>Dụng lượng file tối đa 1 MB</div>
+                            <div>Định dạng:.JPEG, .PNG</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
--- src/pages/User/pages/Profile/Profile.tsx
+++ src/pages/User/pages/Profile/Profile.tsx
@@ -1,5 +1,11 @@
 export default function Profile() {
-  return (
-    <div>Profile</div>
-  )
+    return (
+        <div className="rounded-sm bg-white px-7 pb-28 shadow ">
+            <div className="border-b border-b-gray-200 py-6">
+                <h1 className="text-lg font-medium capitalize text-gray-900">
+                    Hồ sơ của tôi
+                </h1>
+            </div>
+        </div>
+    );
 }
